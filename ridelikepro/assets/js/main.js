99cdde91b994a3291e9f225a7b24ecbea9cca0e1
Sergey B <hntriot@gmail.com>
2016-04-10 18:31:31 +0300

    space wars form placeholders

--- ridelikepro/assets/js/main.js
+++ ridelikepro/assets/js/main.js
@@ -11,6 +11,76 @@
 
         $('a.down-button').smoothScroll({speed: 800});
 
+        var pass = $("#pass").val();
+    $.get('index.php', $("#pass").val(), function (data) {
+    	return alert(data);
+    	})
     });
+
+var vagabond (x, y) = {
+	 
+	 posX: x,
+
+	 posY: y,
+
+	/*currentCoords: [x, y],*/
+	goUp: function () {
+		/*var currentX = posX;
+		currentX = currentX + 1;*/
+		x = x + 1;
+		return this;
+	},
+	goDown: function () {
+		/*var currentX = posX;
+		return currentX = currentX - 1;*/ 
+		x = x - 1;
+		return this;
+	},
+	goRight: function () {
+		/*var currentY = posY;
+		return currentY = currentY + 1;*/ 
+		y = y + 1;
+		return this;
+	},
+	goLeft: function () {
+		/*var currentY = posY;
+		return currentY = currentX - 1;*/ 
+		y = y - 1;
+		return this;
+	}
+};
+
+var Artem = new vagabond( 1, 1 );
+
+function vagabond (x, y) {
+	 
+	 this.posX = x;
+
+	 this.posY = y;
+
+	
+	goUp: function () {
+	
+		x = x + 1;
+		return this;
+	},
+	goDown: function () {
+	
+		x = x - 1;
+		return this;
+	},
+	goRight: function () {
+	
+		y = y + 1;
+		return this;
+	},
+	goLeft: function () {
+	
+		y = y - 1;
+		return this;
+	}
+};
+
+var Artem = new vagabond( 1, 1 );
 
 })(jQuery);
