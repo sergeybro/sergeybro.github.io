de0a9b16ac4ed8b8c7fe5ed11f103a22908f8b58
sergey <hntriot@gmail.com>
2018-08-27 00:40:45 +0300

    perfectial firs commit: first configuration gulpfile, test

---
+++ perf_test/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+// var pug = require('gulp-pug');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var watch = require('gulp-watch');
+// var concat = require('gulp-concat');
+// var sourcemaps = require('gulp-sourcemaps');
+//
+// gulp.task('html', function(){
+//     return gulp.src('client/templates/*.pug')
+//         .pipe(pug())
+//         .pipe(gulp.dest('build/html'))
+// });
+
+gulp.task('css', function(){
+    return gulp.src('./src/sass/main.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('stream', function () {
+    // Endless stream mode
+    return watch('css/**/*.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('js', function(){
+    return gulp.src('client/javascript/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('default', ['clean', 'css'] /*[ 'html', 'css', 'js' ]*/);
