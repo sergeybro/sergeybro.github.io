930cecfe661d994f87dbf09cccc623a15a89155e
sergey <hntriot@gmail.com>
2018-08-31 00:20:59 +0300

    perfectial: updated gulpfile,added svg icons - email, arrow

--- perf_test/gulpfile.js
+++ perf_test/gulpfile.js
@@ -1,23 +1,22 @@
 var gulp = require('gulp');
-// var pug = require('gulp-pug');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
-var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+// var watch = require('gulp-watch');
 var concat = require('gulp-concat');
-// var sourcemaps = require('gulp-sourcemaps');
-//
-// gulp.task('html', function(){
-//     return gulp.src('client/templates/*.pug')
-//         .pipe(pug())
-//         .pipe(gulp.dest('build/html'))
-// });
+
+var processors = [
+    autoprefixer({browsers: ['last 2 version']})
+];
 
 gulp.task('css', function(){
-    return gulp.src('./src/sass/main.scss')
+    return gulp.src('src/sass/main.scss')
         .pipe(sass())
+        .pipe(postcss(processors))
         .pipe(minifyCSS())
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('clean', function () {
@@ -25,17 +24,25 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
-gulp.task('stream', function () {
-    // Endless stream mode
-    return watch('css/**/*.css', { ignoreInitial: false })
-        .pipe(gulp.dest('build'));
-});
-
 gulp.task('js', function(){
-    return gulp.src('./src/js/*.js')
+    return gulp.src('src/js/*.js')
         .pipe(concat('app.js'))
-        // .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('default', ['clean', 'css', 'js'] /*[ 'html', 'css', 'js' ]*/);
+gulp.task('copy', function(){
+    return gulp.src([
+        'src/assets/*.{jpg,png,jpeg,svg,gif}'
+    ])
+        .pipe(gulp.dest('dist/assets'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.scss', ['css']);
+    gulp.watch('src/**/*.js', ['js']);
+});
+
+//gulp.task('default', ['clean', 'css', 'js'] /*[ 'html', 'css', 'js' ]*/);
+
+gulp.task('build', ['css', 'js', 'copy']);
+gulp.task('default', ['clean', 'build']);
